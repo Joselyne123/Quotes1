e45f44710beba86642d2ec658e68426eba300242
joseingabire123@gmail.com <joseingabire123@gmail.com>
2020-11-02 14:14:07 +0200

    add pragraph

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,4 @@
-export class uote {
+export class Quote {
     showAuthor: boolean;
     constructor(public id: number,public name: string,public author: string,  public like: number, public unlike: number,public completeDate: Date){
       this.showAuthor=false;
