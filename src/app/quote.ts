49c9c3fa8c5665da122bbde5dfc21c5af2669acf
joseingabire123@gmail.com <joseingabire123@gmail.com>
2020-11-02 14:05:39 +0200

    add constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,4 @@
-export class Quote {
+export class uote {
     showAuthor: boolean;
     constructor(public id: number,public name: string,public author: string,  public like: number, public unlike: number,public completeDate: Date){
       this.showAuthor=false;
